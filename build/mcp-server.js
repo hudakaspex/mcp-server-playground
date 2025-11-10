8c3f81179607ee3ef423effee444594cd4a4b218
mifta <miftakhul348@gmail.com>
2025-11-10 23:59:19 +0700

    add project

---
+++ build/mcp-server.js
@@ -0,0 +1,20 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
+export const server = new McpServer({
+    name: "mcp-playground",
+    version: "1.0.0",
+});
+// https://modelcontextprotocol.io/specification/2025-06-18/server/tools
+server.registerTool("simple-calculation", {
+    title: "Calculate Sum",
+    description: "Calculates the sum of two numbers.",
+    inputSchema: {
+        num1: z.number(),
+        num2: z.number(),
+    },
+}, async ({ num1, num2 }) => {
+    const output = { result: num1 + num2 };
+    return {
+        content: [{ type: "text", text: JSON.stringify(output) }],
+    };
+});
