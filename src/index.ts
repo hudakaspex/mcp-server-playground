b9f349b60ec32940028997624add6286c856a3fa
mifta <miftakhul348@gmail.com>
2025-11-13 00:28:22 +0700

    init push

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ app.post("/", async (req: Request, res: Response) => {
     await server.connect(transport);
     await transport.handleRequest(req, res, req.body);
 });
-
 const port = parseInt(process.env.PORT || "3000");
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
