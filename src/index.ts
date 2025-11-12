fbb7a369755a7ce1fe22b3b399904dfcd162b1af
mifta <miftakhul348@gmail.com>
2025-11-12 23:00:41 +0700

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { server } from "./mcp-server.js";
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 
-function main() {
+async function main() {
     const transport = new StdioServerTransport();
-    return server.connect(transport);
+    await server.connect(transport);
 }
 
 main()
